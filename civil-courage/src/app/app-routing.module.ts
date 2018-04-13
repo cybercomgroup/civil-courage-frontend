db410748ffb41cc56881a7a2e97b4e77262824f7
Edholm92 <henrik.edholm92@gmail.com>
2018-04-14 00:51:22 +0200

    Added login page + bootstrap support

--- civil-courage/src/app/app-routing.module.ts
+++ civil-courage/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { LoginComponent } from './modules/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
