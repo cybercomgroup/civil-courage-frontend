6e25bb138aff7dca68474221c3be9f2e893d850a
Edholm92 <henrik.edholm92@gmail.com>
2018-04-13 23:45:26 +0200

    Added template components for dashboard and login

--- civil-courage/src/app/app.module.ts
+++ civil-courage/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { LoginComponent } from './modules/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
